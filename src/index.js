63d3a4fcd0ee8d1a185e77d447c931ae5c810a41
AmaanAnis <amaananis99@gmail.com>
2026-05-17 19:47:14 +0500

    corrected redis method params, live leaderboard

--- src/index.js
+++ src/index.js
@@ -19,21 +19,24 @@ app.post("/post/:id/view", async (req, res) => {
 })
 
 app.post("/leaderboard/score", async(req, res) => {
-    const key = req.body.leaderboard
-    const score = req.body.score
-    const member = req.body.member
+    const { leaderboard, score, member } = req.body
 
-    const incrScore = await redis.zincrby(key, score, member) 
+    const incrScore = await redis.zincrby(leaderboard, score, member) 
     res.json({redis:incrScore})
 })
 
-app.get("/leaderboard", async(req, res) => {
-    const topTen = await redis.zrevrange(1, 10)
+app.get("/leaderboard/:players", async(req, res) => {
+
+    const {players} = req.params
+
+    const topTen = await redis.zrevrange(leaderboard, 0, 9, "WITHSCORES")
     res.json({redis:topTen})
 })
 
-app.get("/leaderboard/:userid/rank", async (req, res) => {
-    const userRank = await redis.zrevrank() 
+app.get("/leaderboard/:players/:userid/rank", async (req, res) => {
+
+    const {players, userid} = req.params
+    const userRank = await redis.zrevrank(players, userid) 
     res.json({redis:userRank})
 })
 
