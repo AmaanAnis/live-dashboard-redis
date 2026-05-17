6600c076b12810147717ead06b8e8078131055fa
AmaanAnis <amaananis99@gmail.com>
2026-05-17 18:59:08 +0500

    made all routes, testing

--- src/index.js
+++ src/index.js
@@ -12,6 +12,31 @@ app.get("/redis", async (req, res) => {
     res.json({redis:reply});
 })
 
+app.post("/post/:id/view", async (req, res) => {
+    const newVal = await redis.incr("post-view-counter");
+    res.json({redis:newVal})
+    
+})
+
+app.post("leaderboard/score", async(req, res) => {
+    const key = req.body.leaderboard
+    const score = req.body.score
+    const member = req.body.member
+
+    const incrScore = await redis.zincrby(key, score, member) 
+    res.json({redis:incrScore})
+})
+
+app.get("leaderboard", async(req, res) => {
+    const topTen = await redis.zrevrange(1, 10)
+    res.json({redis:topTen})
+})
+
+app.get("leaderboard/:userid/rank", async (req, res) => {
+    const userRank = await redis.zrevrank() 
+    res.json({redis:userRank})
+})
+
 app.listen(3000, () => {
     console.log("server is running on port 3000");
 });
